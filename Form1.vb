2870841ea597c30ba087f762c48972e108fe4634
abhijeeetdeshmukh9 <68593347+abhijeeetdeshmukh9@users.noreply.github.com>
2020-08-02 22:39:12 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        HookApplication(TextBox1.Text)
+
+        MsgBox("Success")
+    End Sub
+End Class
